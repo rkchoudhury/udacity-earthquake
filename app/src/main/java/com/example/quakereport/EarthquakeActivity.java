b84ce001c5ca59e380115aa609664d9e180731dc
Rakesh Choudhury <RAC8798@bswhealth.org>
2021-05-30 12:11:11 +0530

    Integrate API to fetch all the earthquake data for the initial screen

--- app/src/main/java/com/example/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/quakereport/EarthquakeActivity.java
@@ -3,6 +3,7 @@ package com.example.quakereport;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,28 +21,32 @@ import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
+    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-12-01&minmagnitude=6";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earthquake);
 
-        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
+    }
 
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+    private void updateUI(ArrayList<Earthquake> earthquakes) {
+        ListView earthquakeListView = findViewById(R.id.list);
 
         EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
-
         earthquakeListView.setAdapter(adapter);
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
-                sendIntent.setData(Uri.parse("https://stackoverflow.com/questions/43980740/open-url-in-intent/43981160"));
-
-                if (sendIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(sendIntent);
-                }
+//                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+//                sendIntent.setData(Uri.parse("https://stackoverflow.com/questions/43980740/open-url-in-intent/43981160"));
+//
+//                if (sendIntent.resolveActivity(getPackageManager()) != null) {
+//                    startActivity(sendIntent);
+//                }
 
 //                try {
 //                    Uri webpage = Uri.parse("https://stackoverflow.com/questions/43980740/open-url-in-intent/43981160");
@@ -53,6 +58,25 @@ public class EarthquakeActivity extends AppCompatActivity {
 //                }
             }
         });
+    }
 
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, ArrayList<Earthquake>>{
+
+        @Override
+        protected ArrayList<Earthquake> doInBackground(String... strings) {
+            if (strings.length < 1 || strings[0] == null) {
+                return null;
+            }
+            ArrayList<Earthquake>  earthquakeList = QueryUtils.getEarthquakeData(strings[0]);
+            return earthquakeList;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Earthquake> earthquakes) {
+            if (earthquakes == null) {
+                return;
+            }
+            updateUI(earthquakes);
+        }
     }
 }
